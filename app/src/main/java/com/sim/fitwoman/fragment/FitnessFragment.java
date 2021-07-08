fa0252fb8bee922f529a09065c010f644eaefa8f
Koomeh <koomehjoan@gmail.com>
2021-07-09 00:41:08 +0300

    changes string request to jsonrequest Fitness Fragment

--- app/src/main/java/com/sim/fitwoman/fragment/FitnessFragment.java
+++ app/src/main/java/com/sim/fitwoman/fragment/FitnessFragment.java
@@ -20,6 +20,7 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.sim.fitwoman.MFitnessSingleWorkout;
@@ -112,14 +113,13 @@ public class FitnessFragment extends Fragment {
     private void loadActivities() {
 
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, WORK_OUTS_API_URL,
-                new Response.Listener<String>() {
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, WORK_OUTS_API_URL,null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
+                    public void onResponse(JSONObject response) {
                         try {
                             //converting the string to json array object
-                            JSONArray array = new JSONArray(response);
-
+                            JSONArray array = response.getJSONArray("workouts");
                             //traversing through all the object
                             for (int i = 0; i < array.length(); i++) {
 
